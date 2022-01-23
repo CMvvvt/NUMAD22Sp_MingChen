e932030c9e2ff992761b92e366d7c1070d1c46d9
Ming Chen <452000761@qq.com>
2022-01-23 15:49:03 -0800

    feat: icon and button added

--- app/src/main/java/edu/neu/madcourse/numad22sp_mingchen/MainActivity.kt
+++ app/src/main/java/edu/neu/madcourse/numad22sp_mingchen/MainActivity.kt
@@ -2,10 +2,18 @@ package edu.neu.madcourse.numad22sp_mingchen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun buttonInfo(view: android.view.View) {
+        Toast.makeText(this, "Ming Chen, chen.ming1@northeastern.edu", Toast.LENGTH_SHORT).show();
     }
 }
